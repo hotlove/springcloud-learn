a241097fb9a6ecf7b74eeba54f2e82f74d96f2a0
hotlove_linx <guolejian@126.com>
2020-08-27 23:02:21 +0800

    eureka添加

---
+++ cloud-eureka-server7001/src/main/java/com/guo/springcloud/EurekaMain7001.java
@@ -0,0 +1,18 @@
+package com.guo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Auther: hotlove_linx
+ * @Date: 2020/8/27 22:49
+ * @Description:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}
