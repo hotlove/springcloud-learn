df9215d5265c9abee16fff2cc2b7483335c3cdb9
hotlove_linx <guolejian@126.com>
2020-08-28 22:18:05 +0800

    eureka添加

--- cloud-consumer-order80/src/main/java/com/guo/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/guo/springcloud/OrderMain80.java
@@ -2,6 +2,7 @@ package com.guo.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Auther: hotlove_linx
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Description:
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
