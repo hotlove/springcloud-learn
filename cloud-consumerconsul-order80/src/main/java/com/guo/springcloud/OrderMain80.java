5578a8c68fdf9985ba59a07d126eb8b2a8b1a6b3
hotlove_linx <guolejian@126.com>
2020-09-17 22:38:21 +0800

    增加consul注册中心

---
+++ cloud-consumerconsul-order80/src/main/java/com/guo/springcloud/OrderMain80.java
@@ -0,0 +1,18 @@
+package com.guo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Auther: hotlove_linx
+ * @Date: 2020/9/17 22:37
+ * @Description:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class, args);
+    }
+}
---
+++ cloud-providerconsul-payment8006/src/main/java/com/guo/springcloud/PaymentMain8006.java
@@ -0,0 +1,18 @@
+package com.guo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Auther: hotlove_linx
+ * @Date: 2020/9/17 22:28
+ * @Description:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8006 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8006.class, args);
+    }
+}
