20b2752249619bf49bdffbbd0da1f916afdb3fb3
hotlove_linx <guolejian@126.com>
2020-11-09 22:45:57 +0800

    1.alibaba nacos

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/guo/springcloud/PaymentMain9001.java
@@ -0,0 +1,18 @@
+package com.guo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Auther: hotlove_linx
+ * @Date: 2020/11/9 22:25
+ * @Description:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+    }
+}
---
+++ cloudalibaba-provider-payment9001/src/main/java/com/guo/springcloud/controller/PaymentController.java
@@ -0,0 +1,23 @@
+package com.guo.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther: hotlove_linx
+ * @Date: 2020/11/9 22:27
+ * @Description:
+ */
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id) {
+        return "nacos registry, serverport"+serverPort+"\t id" + id;
+    }
+}
