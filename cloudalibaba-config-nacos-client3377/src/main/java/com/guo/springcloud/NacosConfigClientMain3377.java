e6f023ffe336d20a464c36d393d688176039981d
hotlove_linx <guolejian@126.com>
2020-11-22 17:30:04 +0800

    1.alibaba nacos

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/guo/springcloud/NacosConfigClientMain3377.java
@@ -0,0 +1,18 @@
+package com.guo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Auther: hotlove_linx
+ * @Date: 2020/11/22 14:04
+ * @Description:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
