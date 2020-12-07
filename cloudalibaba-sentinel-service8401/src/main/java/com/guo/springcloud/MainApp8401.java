f64f6a31bf952dab4dd3b3752fe4390464dbda00
hotlove_linx <guolejian@126.com>
2020-12-07 22:47:21 +0800

    1.alibaba nacos

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/guo/springcloud/MainApp8401.java
@@ -0,0 +1,18 @@
+package com.guo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Auther: hotlove_linx
+ * @Date: 2020/12/7 22:25
+ * @Description:
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MainApp8401 {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp8401.class, args);
+    }
+}
---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/guo/springcloud/controller/FlowLimitController.java
@@ -0,0 +1,27 @@
+package com.guo.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther: hotlove_linx
+ * @Date: 2020/12/7 22:26
+ * @Description:
+ */
+@RestController
+@Slf4j
+public class FlowLimitController {
+    @GetMapping("/testA")
+    public String testA()
+    {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB()
+    {
+        log.info(Thread.currentThread().getName()+"\t"+"...testB");
+        return "------testB";
+    }
+}
