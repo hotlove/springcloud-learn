4d2e9a23c8a53790911bdf1b6178b306c6160b10
hotlove_linx <guolejian@126.com>
2021-03-18 22:49:23 +0800

    1.引入搜索服务

---
+++ cloud-search9200/src/main/java/com/guo/springcloud/Search9200.java
@@ -0,0 +1,18 @@
+package com.guo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Auther: hotlove_linx
+ * @Date: 2021/3/18 22:46
+ * @Description:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Search9200 {
+    public static void main(String[] args) {
+        SpringApplication.run(Search9200.class, args);
+    }
+}
---
+++ cloud-search9200/src/main/java/com/guo/springcloud/config/ElasticSearchConfig.java
@@ -0,0 +1,14 @@
+package com.guo.springcloud.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Auther: hotlove_linx
+ * @Date: 2021/3/18 22:47
+ * @Description:
+ */
+@Configuration
+public class ElasticSearchConfig {
+
+
+}
